0d357b762b0077cd7ce97c2eb57a3c5c97b791c0
Madhav Shukla <madhavshuklasr3@gmail.com>
2022-03-19 17:47:06 +0530

    search fileter in get hospitals and added city in hospital modal

--- middlewares/hospitalDetails.js
+++ middlewares/hospitalDetails.js
@@ -61,16 +61,35 @@ exports.getDetailsById = async (req, res) => {
 
 //todo GET ALL THE HOSPITAL DETAILS
 exports.getAllHospitals = async (req, res) => {
-  try {
-    const hospitals = await Hospital.find();
-    if (hospitals.length <= 0) {
-      return res.status(400).json({ message: 'No Hospitals found' });
-    } else {
-      return res.status(200).json(hospitals);
+  if (req.query.length > 0) {
+    console.log(req.query);
+
+    try {
+      const hospitals = await Hospital.find({
+        name: req.query.name,
+        city: req.query.city,
+      });
+      if (hospitals.length <= 0) {
+        return res.status(400).json({ message: 'No Hospitals found' });
+      } else {
+        return res.status(200).json(hospitals);
+      }
+    } catch (error) {
+      console.log(error);
+      return res.status(500).json({ message: 'Internal Server error!' });
     }
-  } catch (error) {
-    console.log(error);
-    return res.status(500).json({ message: 'Internal server error' });
+  } else {
+    try {
+      const hospitals = await Hospital.find();
+      if (hospitals.length <= 0) {
+        return res.status(400).json({ message: 'No Hospitals found' });
+      } else {
+        return res.status(200).json(hospitals);
+      }
+    } catch (error) {
+      console.log(error);
+      return res.status(500).json({ message: 'Internal server error' });
+    }
   }
 };
 
@@ -82,12 +101,12 @@ exports.addDoctors = async (req, res) => {
 
   try {
     const updated = await Hospital.findByIdAndUpdate(
-      { _id : userId },
+      { _id: userId },
       { $push: { doctors: doctors } }
-    )
-    return res.status(200).json(updated); 
+    );
+    return res.status(200).json(updated);
   } catch (error) {
-    console.log(error) ;
+    console.log(error);
     return res.status(500).json({
       message: 'Internal server error',
     });
